e5e7c8776063971ac8df164d76d7a60a96d6fe32
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-09-13 15:45:39 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,10 +24,6 @@ def can_be_created_with_a_hash_of_attributes
   movie= Movie.new(attributes)
   movie.save
   movie
-
-  movie = attributes
-  movie= Movie.new(attributes)
-
 end
 
 def can_be_created_in_a_block(args ={title: "Home Alone", release_date: 1990})
